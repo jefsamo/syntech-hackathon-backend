a1e41457e3b0a7aa173adeeb051186d608f2c015
Olawale Shopeyin <shopeyinwale@gmail.com>
2025-11-09 05:38:17 +0000

    main.ts update

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ExpiryModule } from './expiry/expiry.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ExpiryModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true, cache: true }),
+    ExpiryModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
